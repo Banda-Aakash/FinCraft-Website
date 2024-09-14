4a44b90dce935356d20c095b3038a9e5f48c2ac6
Banda-Aakash <aakashbanda03@gmail.com>
2024-09-15 04:20:45 +0530

    Home Page changes 1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,75 @@
-import React from "react";
-import "./Home.css";
-
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
 const Home = () => {
-  return (
-    <div className="home-page">
-        Home page
-    </div>
-  );
+    return (
+        <div className="home-page">
+            <div className="content">
+                <h6 className='heading'>Investments made faster with FinCraft</h6>
+
+                <Link to="https://play.google.com" className="download-button">
+                    <img
+                        src="Playstore.png"
+                        alt="Google Play"
+                        className="button-logo"
+                    />
+                    <div className="button-text">
+                        <span className="platform">For Android</span>
+                        <span className="download-now">Download now</span>
+                    </div>
+                </Link>
+
+                {/* <a href="https://play.google.com" className="download-button">
+                    <img
+                        src="Playstore.png"
+                        alt="Google Play"
+                        className="button-logo"
+                    />
+                    <div className="button-text">
+                        <span className="platform">For Android</span>
+                        <span className="download-now">Download now</span>
+                    </div>
+                </a> */}
+
+                <p className='Partners'>Our Partners</p>
+
+                <Link to="https://play.google.com" className="download-button">
+                    <img
+                        src="Playstore.png"
+                        alt="Google Play"
+                        className="button-logo"
+                    />
+                    <div className="button-text">
+                        <span className="platform">For Android</span>
+                        <span className="download-now">Download now</span>
+                    </div>
+                </Link>
+                <Link to="https://play.google.com" className="download-button">
+                    <img
+                        src="Playstore.png"
+                        alt="Google Play"
+                        className="button-logo"
+                    />
+                    <div className="button-text">
+                        <span className="platform">For Android</span>
+                        <span className="download-now">Download now</span>
+                    </div>
+                </Link>
+                <Link href="https://play.google.com" className="download-button">
+                    <img
+                        src="Playstore.png"
+                        alt="Google Play"
+                        className="button-logo"
+                    />
+                    <div className="button-text">
+                        <span className="platform">For Android</span>
+                        <span className="download-now">Download now</span>
+                    </div>
+                </Link>
+            </div>
+            <div>ihi</div>
+        </div>
+    );
 };
 
 export default Home;
