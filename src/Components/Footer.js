d4243109b39532c9c627e6770def1f76fc2020b5
Banda-Aakash <aakashbanda03@gmail.com>
2024-09-15 04:26:19 +0530

    Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import './Footer.css'
-function Footer() {
+import React from "react";
+import "./Footer.css"; // Make sure to create this CSS file
+import { Link } from "react-router-dom";
+const Footer = () => {
   return (
-    <div className='footer'>
-      <p>Get in touch</p>
-    </div>
-  )
-}
+    <footer className="footer-container">
+      <div className="footer-links">
+        <Link to="/privacy-policy" className="footer-link">
+          Privacy Policy
+        </Link>
+        <Link to="/terms-of-use" className="footer-link">
+          Terms of Use
+        </Link>
+      </div>
 
-export default Footer
+      <div className="social-media">
+        <Link to="https://facebook.com" target="_blank" rel="noreferrer">
+          <img src="FbLogo.png" alt="Facebook" />
+        </Link>
+        <Link to="https://instagram.com" target="_blank" rel="noreferrer">
+          <img src="InstagramLogo.png" alt="Instagram" />
+        </Link>
+        <Link to="https://linkedin.com" target="_blank" rel="noreferrer">
+          <img src="LinkedinLogo.png" alt="LinkedIn" />
+        </Link>
+        <Link to="https://twitter.com" target="_blank" rel="noreferrer">
+          <img src="TwitterLogo.png" alt="Twitter" />
+        </Link>
+      </div>
+
+      <div className="footer-company">
+        <p>© 2024, Fincraft Technology Pvt Ltd.</p>
+        <p>
+          Address – 4th Floor, Building, 387, Koramangala 4th Block, Koramangala, Bengaluru, Karnataka 560034
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
